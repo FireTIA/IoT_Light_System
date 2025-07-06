e26e3ae474e42b37da18730214dc808aa64d8fc7
FireTIA <78191676+FireTIA@users.noreply.github.com>
2025-07-06 16:28:55 +0300

    Delete modules directory

--- modules/datetime_helper.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-def get_formatted_datetime():
-    """Возвращает текущую дату и время в формате [DD.MM.YYYY-HH:MM]"""
-    now = datetime.now()
-    return f"[{now.strftime('%d.%m.%Y-%H:%M')}]"
-
--- modules/lock_pc.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def run():
-    print("Блокирую компьютер.")
-    try:
-        os.system("rundll32.exe user32.dll,LockWorkStation")
-    except Exception as e:
-        print("Ошибка при блокировке ПК:", e)
